64622b675af7808a78747d5072f8723ab984d28f
hyunny82 <hyunny82@gmail.com>
2021-10-26 05:27:06 +0900

    udpate

---
+++ src/types/index.d.ts
@@ -0,0 +1,20 @@
+interface IUser {
+  _id?: string;
+  userId: string;
+  name: string;
+  password: string;
+  role: string;
+}
+
+interface IERROR_CODE_TABLE {
+  [key: string]: string;
+}
+
+type TERROR_RESPONSE = {
+  error: string;
+  message: string;
+  detail: string;
+};
+
+type TERROR_CODE = string | undefined;
+type TERROR_MESSAGE = string | undefined;
