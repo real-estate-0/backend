ebe9d02897f0d4bd6ebd7a5c01def0ee1469b8b2
hyunny88 <hyunny82@gmail.com>
2021-11-26 15:55:04 +0900

    udpate

---
+++ test.py
@@ -0,0 +1,23 @@
+"""
+import requests
+
+url = 'http://apis.data.go.kr/1741000/StanReginCd/getStanReginCdList'
+params ={'serviceKey' : 'UQoQhO/CpOPm65pe+obx1jBuKFhT+2tXx2jIFwwsrkp5Q/TfZw8hYAv3j4hSN+n0Cs35+6ZeuKGGGb07pX+qCg==', 
+            'pageNo' : '1', 
+            'numOfRows' : '3', 
+            'type' : 'json', 
+            'locatadd_nm' : '문정동' 
+        }
+
+response = requests.get(url, params=params)
+print(response.content)
+"""
+
+import requests
+
+url = 'http://apis.data.go.kr/1611000/nsdi/ReferLandPriceService/attr/getReferLandPriceAttr'
+params ={'serviceKey' : 'UQoQhO/CpOPm65pe+obx1jBuKFhT+2tXx2jIFwwsrkp5Q/TfZw8hYAv3j4hSN+n0Cs35+6ZeuKGGGb07pX+qCg==', 
+'ldCode' : '1111017400', 'stdrYear' : '2021', 'format' : 'json', 'numOfRows' : '10', 'pageNo' : '1' }
+
+response = requests.get(url, params=params)
+print(response.content)
